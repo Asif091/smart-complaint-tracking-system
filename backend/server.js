29a98a1704447d7b212a06871902e8605babc1d9
Asif Rahman <srasif131@gmail.com>
2026-03-06 16:37:35 +0600

    added MongoDB connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
+
+// connect database
+connectDB();
 
 // middleware
 app.use(cors());
@@ -9,11 +16,11 @@ app.use(express.json());
 
 // test route
 app.get("/", (req, res) => {
-    res.send("Smart Complaint Tracking System Backend Running");
+  res.send("Smart Complaint Tracking System Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
